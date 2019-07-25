473f11fde092b70739fc45c6b2884dfcccc98b0a
creepysta <sweetnanispregnantghost@yahoo.com>
2019-07-26 01:37:57 +0530

    leaderboard option in navigation view added

--- app/src/main/java/com/sam/guessinggame/ModeSelection.java
+++ app/src/main/java/com/sam/guessinggame/ModeSelection.java
@@ -2,13 +2,22 @@ package com.sam.guessinggame;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class ModeSelection extends AppCompatActivity {
 
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
     private Context context;
 
     @Override
@@ -16,6 +25,29 @@ public class ModeSelection extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mode_selection);
         context = this;
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_menu, R.string.close_menu);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.leaderBoard:
+                        startActivity(new Intent(ModeSelection.this, LeaderBoard.class));
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+
     }
 
     public void onSelectAnimal(View view) {
@@ -30,6 +62,13 @@ public class ModeSelection extends AppCompatActivity {
         Intent intent = new Intent(context, FlowerMode.class);
         intent.putExtra("cred", cred);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 
 }
